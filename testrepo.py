3048971d2ac11d38d33fbe8f30d1ce14a8709575
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 22:01:51 -0500

    Update testrepo.py

--- testrepo.py
+++ testrepo.py
@@ -4,4 +4,4 @@ nouns = ("puppy", "car", "rabbit", "girl", "monkey")
 verbs = ("runs", "hits", "jumps", "drives", "barfs")
 adj = ("adorable", "clueless", "dirty", "odd", "stupid")
 num = random.randrange(0,5)
-print nouns[num] + ' ' + verbs[num] + ' ' + adv[num] + ' ' + adj[num]
+print nouns[num] + ' ' + verbs[num] + ' ' + adj[num]
