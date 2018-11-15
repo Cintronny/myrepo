09acefd3a90a1f6a7eb9d8b6e0e89809898b40a2
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 22:01:18 -0500

    Update testrepo.py

--- testrepo.py
+++ testrepo.py
@@ -2,7 +2,6 @@ import random
 
 nouns = ("puppy", "car", "rabbit", "girl", "monkey")
 verbs = ("runs", "hits", "jumps", "drives", "barfs")
-adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
 adj = ("adorable", "clueless", "dirty", "odd", "stupid")
 num = random.randrange(0,5)
 print nouns[num] + ' ' + verbs[num] + ' ' + adv[num] + ' ' + adj[num]
