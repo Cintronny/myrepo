05a8e22297bd0e29347c445bd76d6861349e9070
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 21:44:15 -0500

    Update testrepo.py

--- testrepo.py
+++ testrepo.py
@@ -1,7 +1,7 @@
 import random
 
-nouns = ("Tuition", "students", "college", "fees", "university", "semester", "dollars", "student", "We’re", "money", "time", "Semester", "Cashapp", "Student", "Tuition", "Tuition", "PayPal", "people", "school", "books", "class", "email", "we’re", "year")
+nouns = ("Tuition", "students", "college", "fees", "university", "semester", "dollars", "student", "money", "time", "Semester", "Cashapp", "Student", "Tuition", "Tuition", "PayPal", "people", "school", "books", "class", "email", "we’re", "year")
 verbs = ("pay", "will", "make", "education", "fighting", "ability", "money", "needs", "help", "can", "win", "looking", "Thanks", "access", "attend", "don’t", "gonna", "learn", "just")
 adj = ("free", "just", "many", "new")
 num = random.randrange(0,24)
-print nouns[num] + ' ' + verbs[num] + ' ' + adv[num] + ' ' + adj[num]
+print adj[num] + ' ' + verbs[num] + ' ' + nouns[num]
