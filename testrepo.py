2867bc1d9630c8f764bbf18b7e185329fe70e946
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 22:30:42 -0500

    Update testrepo.py

--- testrepo.py
+++ testrepo.py
@@ -1,11 +1,14 @@
 import random
 
 nouns = ("tuition", "students", "college", "fees", "university", "semester", "school", "books", "class")
-verbs = ("pay", "will", "make", "fighting", "ability", "helps", "can", "access", "attending", "don't", "gonna", "learn")
+verbs = ("pay", "will", "make", "fighting", "helps", "can", "access", "attending", "don't", "gonna", "learn")
 adj = ("free", "many", "new")
-num = random.randrange(0,9)
-vernum = random.randrange(0,12)
-adnum = random.randrange(0,3)
-first_half = (nouns[num] + ' ' + verbs[vernum] + ' ' + adj[adnum])
-second_half = (adj[adnum] + ' ' + verbs[vernum] + ' ' + nouns[num])
+num = random.randrange(0:4)
+num_two = random.range(5:9)
+vernum = random.randrange(0:5)
+vernum_two = random.randrange(6:11)
+adnum = random.randrange(0:1)
+adnum_two = random.randrange(2:3)
+first_half = (adj[adnum] + ' ' + nouns[num] + ' ' + verbs[vernum])
+second_half = (adj[adnum_two] + ' ' + verbs[vernum_two] + ' ' + nouns[num_two])
 print first_half + ' ' + second_half
